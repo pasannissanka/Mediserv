42e3a8844f2561fe6cd641f3ca35a2a35249584e
pasannissanka <pasannissanka@hotmail.com>
2021-09-01 15:25:53 +0530

    init repo

---
+++ mediserv-api/src/main/java/com/example/mediservapi/MediservApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediservapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediservApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediservApiApplication.class, args);
+	}
+
+}
---
+++ mediserv-api/src/test/java/com/example/mediservapi/MediservApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediservapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediservApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
