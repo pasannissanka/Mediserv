c13a4cb081e2024e72e496b8a58d1b294c3375fa
primesh <primeshheshan@gmail.com>
2021-09-04 16:49:59 +0530

    MediServApiApplication class reset

--- mediserv-api/src/main/java/com/example/mediservapi/MediServApiApplication.java
+++ mediserv-api/src/main/java/com/example/mediservapi/MediServApiApplication.java
@@ -12,10 +12,4 @@ public class MediServApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MediServApiApplication.class, args);
 	}
-
-	@GetMapping
-	public String hello() {
-		return "Hello world";
-	}
-
 }
