f8688472ed776517d1c14503011ca42974c5940f
primesh <primeshheshan@gmail.com>
2021-09-04 18:02:46 +0530

    CustomerService class created in service package

--- mediserv-api/src/main/java/com/example/mediservapi/controller/CustomerController.java
+++ mediserv-api/src/main/java/com/example/mediservapi/controller/CustomerController.java
@@ -1,4 +1,13 @@
 package com.example.mediservapi.controller;
 
+import com.example.mediservapi.service.CustomrService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
+
+    private CustomrService customerService;
+
 }
