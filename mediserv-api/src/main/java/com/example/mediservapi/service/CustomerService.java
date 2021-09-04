0aea614edb7ff47f2ca55a0ef433e70c96d6f9c9
primesh <primeshheshan@gmail.com>
2021-09-04 21:26:18 +0530

    fix updateProfile bug

---
+++ mediserv-api/src/main/java/com/example/mediservapi/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.example.mediservapi.service;
+
+import com.example.mediservapi.controller.request.CreateUpdateCustomerRequest;
+import com.example.mediservapi.controller.request.CreateUpdateUserRequest;
+import com.example.mediservapi.model.customer.Customer;
+import com.example.mediservapi.model.user.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerService {
+    Customer signUp(CreateUpdateCustomerRequest customerData);
+    Optional<Customer> findByEmail(String email);
+    Optional<Customer> findById(String id);
+    Customer updateProfile(String id, CreateUpdateCustomerRequest userData);
+    List<Customer> findAll();
+
+}
