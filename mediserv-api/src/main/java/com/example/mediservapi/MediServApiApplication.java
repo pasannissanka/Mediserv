ed9e2a2382e7b5d50490d54154eb4c4ce37612e2
pasannissanka <pasannissanka@hotmail.com>
2021-09-05 23:29:54 +0530

    added spring corsConfigurer

--- mediserv-api/src/main/java/com/example/mediservapi/MediServApiApplication.java
+++ mediserv-api/src/main/java/com/example/mediservapi/MediServApiApplication.java
@@ -2,6 +2,9 @@ package com.example.mediservapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MediServApiApplication {
@@ -10,4 +13,13 @@ public class MediServApiApplication {
 		SpringApplication.run(MediServApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
