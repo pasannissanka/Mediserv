f5df1ca577e006fd36061ebe70993b25dd19be69
primesh <primeshheshan@gmail.com>
2021-09-07 13:21:29 +0530

    CustomerService done

--- mediserv-api/src/main/java/com/example/mediservapi/service/CustomerService.java
+++ mediserv-api/src/main/java/com/example/mediservapi/service/CustomerService.java
@@ -4,11 +4,12 @@ import com.example.mediservapi.controller.request.CreateUpdateCustomerRequest;
 import com.example.mediservapi.controller.request.CreateUpdateUserRequest;
 import com.example.mediservapi.model.customer.Customer;
 import com.example.mediservapi.model.user.User;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
-public interface CustomerService {
+@Service
+public interface CustomerService { // db ekata data danne
     Customer signUp(CreateUpdateCustomerRequest customerData);
     Optional<Customer> findByEmail(String email);
     Optional<Customer> findById(String id);
