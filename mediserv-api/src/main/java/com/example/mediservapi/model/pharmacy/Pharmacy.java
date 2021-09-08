05303dd729614deeb13e5432cd72b6e238925b03
primesh <primeshheshan@gmail.com>
2021-09-08 07:27:43 +0530

    Pharmacy model addded email

--- mediserv-api/src/main/java/com/example/mediservapi/model/pharmacy/Pharmacy.java
+++ mediserv-api/src/main/java/com/example/mediservapi/model/pharmacy/Pharmacy.java
@@ -4,16 +4,23 @@ import com.example.mediservapi.model.address.Address;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Data
 @Accessors(chain = true)
-@Document("Pharmacies")
+@Document(collection = "Pharmacies")
+
 public class Pharmacy {
     @Id
     private String id;
     private String name;
+
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING)
+    private String email;
+    
     private String description;
     private Address address;
 }
