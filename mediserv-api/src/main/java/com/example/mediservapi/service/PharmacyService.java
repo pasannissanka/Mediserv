e20fc4759fcf7b7e9e2b95ee8b63c47949acaa08
primesh <primeshheshan@gmail.com>
2021-09-08 07:27:12 +0530

    PharmacyService and PharmacyServiceImpl done

--- mediserv-api/src/main/java/com/example/mediservapi/service/PharmacyService.java
+++ mediserv-api/src/main/java/com/example/mediservapi/service/PharmacyService.java
@@ -1,8 +1,18 @@
 package com.example.mediservapi.service;
 
+import com.example.mediservapi.controller.request.CreateUpdatePharmacyRequest;
+import com.example.mediservapi.controller.request.CreateUpdateUserRequest;
 import com.example.mediservapi.dto.model.pharmacy.PharmacyDto;
+import com.example.mediservapi.model.pharmacy.Pharmacy;
+import com.example.mediservapi.model.user.User;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PharmacyService {
-    PharmacyDto createPharmacy(PharmacyDto pharmacyDto);
-    PharmacyDto findOne(String id);
-}
+    Pharmacy signUp(CreateUpdatePharmacyRequest pharmacyData);
+    Optional<Pharmacy>  findByEmail(String email);
+    Optional<Pharmacy> findById(String id);
+    Pharmacy updatePharmacy(String id, CreateUpdatePharmacyRequest pharmacyData);
+    List<Pharmacy> findAll();
+   }
