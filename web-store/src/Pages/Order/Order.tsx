43c1ed6700b174a50afb2ee9fa8950c09293db8f
primesh <primeshheshan@gmail.com>
2021-09-14 16:28:57 +0530

    added Login overlay to order

--- web-store/src/Pages/Order/Order.tsx
+++ web-store/src/Pages/Order/Order.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Login } from "../Login/Login";
 
-type OrderTypes = {};
+//type OrderTypes = {};
+type UserLogedIn = {
+  status: boolean;
+};
 
-export const Order = (props: OrderTypes) => {
+export const Order = () => {
+  function UserlogingStatus() {
+    return true;
+  }
+
+  const [isOpen, setIsOpen] = useState(UserlogingStatus);
   return (
     <>
-      <div className='container mx-auto'>Order</div>
+      <div className="container mx-auto">
+        Order
+        <Login isOpen={isOpen} setIsOpen={setIsOpen} />
+      </div>
     </>
   );
 };
