1d00cfa9cdbb0bae369fb525e7445a4e823129da
primesh <primeshheshan@gmail.com>
2021-09-22 08:56:58 +0530

    File service class implemented

---
+++ mediserv-api/src/main/java/com/example/mediservapi/service/FileService.java
@@ -0,0 +1,58 @@
+package com.example.mediservapi.service;
+
+import com.example.mediservapi.dto.response.LoadPrescription;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.apache.commons.io.IOUtils;
+import java.io.IOException;
+
+//service class contains the service to store and retrieve a file from GridFS.
+@Service
+public class FileService {
+    @Autowired
+    private GridFsTemplate template;
+
+    @Autowired
+    private GridFsOperations operations;
+
+    public String addFile(MultipartFile upload) throws IOException {
+
+        DBObject metadata = new BasicDBObject();
+        metadata.put("fileSize", upload.getSize());
+
+        Object fileID = template.store(upload.getInputStream(), upload.getOriginalFilename(), upload.getContentType(), metadata);
+
+        return fileID.toString();
+    }
+
+    public LoadPrescription downloadFile(String id) throws IOException {
+
+        GridFSFile gridFSFile = template.findOne( new Query(Criteria.where("_id").is(id)) );
+
+        LoadPrescription loadFile = new LoadPrescription();
+
+        if (gridFSFile != null && gridFSFile.getMetadata() != null) {
+            loadFile.setFilename( gridFSFile.getFilename() );
+
+            loadFile.setFileType( gridFSFile.getMetadata().get("_contentType").toString() );
+
+            loadFile.setFileSize( gridFSFile.getMetadata().get("fileSize").toString() );
+
+            loadFile.setFile( IOUtils.toByteArray(operations.getResource(gridFSFile).getInputStream()) );
+        }
+
+        return loadFile;
+    }
+
+
+
+
+}
