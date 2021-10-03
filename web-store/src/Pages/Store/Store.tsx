25b037fb256a124571415386daa9d800562759d1
Dasun <Mikefiver23635@gmail.com>
2021-10-03 23:08:51 +0530

    Useeffect

--- web-store/src/Pages/Store/Store.tsx
+++ web-store/src/Pages/Store/Store.tsx
@@ -1,27 +1,48 @@
-import React from "react";
+import { toUnicode } from "punycode";
+import { useEffect, useState } from "react";
 import { Card } from "../../Components/Card/Card";
 
 type StoreProps = {};
 
 export const Store = (props: StoreProps) => {
+  
+  const [data, setData] = useState(
+    
+  );
+
+  useEffect(() => {
+    async function fetchPharmacyJSON() {
+      const res = await fetch("/api/pharmacies/search", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          page: {
+            limit: 10,
+          },
+          query: {},
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      setData(data);
+    }
+  }, []);
+
   return (
     <>
-      <div className='container mx-auto'>
-        <div className='grid grid-cols-1 gap-6 xl:grid-cols-6 sm:grid-cols-4'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 gap-6 xl:grid-cols-6 sm:grid-cols-4"></div>
+        
+          
+        
       </div>
     </>
   );
 };
+
+function setTodos(data: any) {
+  throw new Error("Function not implemented.");
+}
+
