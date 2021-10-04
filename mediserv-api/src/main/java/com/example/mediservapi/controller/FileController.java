1787df35d7bb2b50e75c4e99e32eae3ec387957c
pasannissanka <pasannissanka@hotmail.com>
2021-10-04 09:12:32 +0530

    bug fixed

--- mediserv-api/src/main/java/com/example/mediservapi/controller/FileController.java
+++ mediserv-api/src/main/java/com/example/mediservapi/controller/FileController.java
@@ -41,10 +41,9 @@ public class FileController {
     @GetMapping("/download/{id}")
     public ResponseEntity<ByteArrayResource> download(@PathVariable String id) throws IOException {
         LoadFile loadFile = fileService.downloadFile(id);
-        // System.out.println(loadFile.getFileType());
-
+        
         return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType("image/jpeg"))
+                .contentType(MediaType.parseMediaType(loadFile.getFileType()))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + loadFile.getFilename() + "\"")
                 .body(new ByteArrayResource(loadFile.getFile()));
     }
